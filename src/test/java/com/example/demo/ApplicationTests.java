b76eda6432440dd33a011bdbb71617bef030f16e
South Mountain <PangGit@users.noreply.github.com>
2023-05-22 23:09:13 +0800

    init

--- src/test/java/com/example/demo/ApplicationTests.java
+++ src/test/java/com/example/demo/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.springboot3;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Springboot3ApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {
