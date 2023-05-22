1d9af35fe68b6548dc7f7a85e6eda73b6858716b
South Mountain <PangGit@users.noreply.github.com>
2023-05-22 23:50:39 +0800

    test

--- src/test/java/com/example/demo/ApplicationTests.java
+++ src/test/java/com/example/demo/ApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
+import com.example.demo.service.JbxxHxxService;
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
 
+    @Resource
+    private JbxxHxxService jbxxHxxService;
+
     @Test
     void contextLoads() {
+        System.out.println("------------Hxx : count = " + jbxxHxxService.count());
     }
 
 }
